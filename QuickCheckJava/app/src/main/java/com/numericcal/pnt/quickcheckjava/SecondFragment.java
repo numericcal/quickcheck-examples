32b2dbd9cf4abb690ff5862ed3906afbfc8e908f
ranko r sredojevic <rrs@numericcal.com>
2020-05-04 17:52:24 -0700

    Default configuration example. User permissions boilerplate.

--- QuickCheckJava/app/src/main/java/com/numericcal/pnt/quickcheckjava/SecondFragment.java
+++ QuickCheckJava/app/src/main/java/com/numericcal/pnt/quickcheckjava/SecondFragment.java
@@ -1,6 +1,6 @@
 package com.numericcal.pnt.quickcheckjava;
 
-import android.icu.util.UniversalTimeScale;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.arch.core.util.Function;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -16,6 +17,7 @@ import com.numericcal.pnt.qc.LiveCameraFeed;
 import com.numericcal.pnt.qc.Messages;
 import com.numericcal.pnt.qc.QuickCheck;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.BiConsumer;
 import java.util.logging.Logger;
@@ -28,6 +30,7 @@ public class SecondFragment extends Fragment {
     private String QUICKCHECK_CLIENT_SECRET = "your service key";
     private View view;
     private static Logger LOGGER = Logger.getLogger("QuicCheckJava-Demo");
+    private static int PERMISSION_REQUEST_CODE = 6699; // if 6 was 9 ... I don't mind ...
 
     @Override
     public View onCreateView(
@@ -37,6 +40,17 @@ public class SecondFragment extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_second, container, false);
 
+        if (allPermissionsGranted()) {
+            setupQuickCheckService();
+        } else {
+            this.requestPermissions(QuickCheck.Static.getREQUIRED_PERMISSIONS(),
+                    PERMISSION_REQUEST_CODE);
+        }
+
+        return view;
+    }
+
+    private void setupQuickCheckService() {
         ////////////////////////////////////////////////////////////////////////////////////////////
         // set up QuickCheck service
         viewFeed = view.findViewById(R.id.quickcheck_live_feed);
@@ -50,21 +64,51 @@ public class SecondFragment extends Fragment {
         qcFuture.whenComplete(new BiConsumer<QuickCheck, Throwable>() {
             @Override
             public void accept(QuickCheck quickcheck, Throwable err) {
-                QuickCheck.Static.QuickCheckException exc = (QuickCheck.Static.QuickCheckException) err;
+                QuickCheck.Static.QuickCheckException exc =
+                        (QuickCheck.Static.QuickCheckException) err;
                 if (err != null) {
                     // error during the QuickCheck initialization
-                    LOGGER.info("Could not initialize QuickCheck: [" + exc.status.toString() + "] " + exc.msg);
+                    LOGGER.info("Could not initialize QuickCheck: [" +
+                            exc.status.toString() + "] " + exc.msg);
                 } else {
                     // QuickCheck service is ready
                     qc = quickcheck;
-                    Toast.makeText(getContext(), "QuickCheck ready!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getContext(), "QuickCheck ready!",
+                            Toast.LENGTH_LONG).show();
 
                     // wire up QuickCheck with the system
                 }
             }
         });
+        // DONE!
+        ////////////////////////////////////////////////////////////////////////////////////////////
+    }
 
-        return view;
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // standard boilerplate
+
+    private boolean allPermissionsGranted() {
+        String[] requests = QuickCheck.Static.getREQUIRED_PERMISSIONS();
+        boolean allTrue = true;
+        for(String request : requests) {
+            allTrue = allTrue &&
+                    ContextCompat.checkSelfPermission(getContext(), request) ==
+                            PackageManager.PERMISSION_GRANTED;
+        }
+        return allTrue;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (allPermissionsGranted()) {
+                setupQuickCheckService();
+            } else {
+                Toast.makeText(getContext(),
+                        "Permissions not granted by the user.", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
